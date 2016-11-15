7b20604c5baf2ac50c8daa619b170b5469f5c0a9
Mohammed Alateyya <mnurulho@andrew.cmu.edu>
2016-11-15 12:52:49 +0300

    Revert "update cut"
    This reverts commit a0338199b008ba59494d6afd4bc208717d108c6c.

--- selection.py
+++ selection.py
@@ -18,7 +18,6 @@ class helperObject:
         self.y1 = None
         self.x2 = None
         self.y2 = None
-        self.selection = (0,0,0,0)
         self.mousex = 0
         self.mousey = 0
         self.state = 0
@@ -31,26 +30,25 @@ class helperObject:
         c.delete(self.square)
         c.delete(self.image_label)
 
-def masterClick(c, e,helper,meta):
+def masterClick(c, e,helper):
     if helper.state==1 and not(helper.x1<e.x<helper.x2 and helper.y1<e.y<helper.y2):
-        helper.clear(c)
-        print 'image',helper.image
-        finalize(helper,meta)
+        finalize()
+        c.delete(helper.square)
         helper.state = 0
     if helper.state == 0:
         helper.x1,helper.y1 = e.x,e.y
+        print 'x1,y1:', helper.x1, helper.y1
     elif helper.state == 1:
-        cut(helper,meta)
         helper.mousex = e.x
         helper.mousey = e.y
         helper.state = 2
+        print 'e.x,e.y: ', e.x, e.y, '  =  '
 
 def masterPressedMotion(c,e,helper):
     if helper.state == 0:
         helper.x2,helper.y2 = e.x,e.y
         c.delete(helper.square)
         helper.square = c.create_rectangle(helper.x1,helper.y1,helper.x2,helper.y2)
-
     if helper.state ==2:
         c.delete(helper.image_label)
         c.delete(helper.square)
@@ -62,10 +60,7 @@ def masterPressedMotion(c,e,helper):
 
 def masterRelease(e,helper,meta):
     if helper.state == 0:
-        if helper.x1>helper.x2: helper.x1,helper.x2=helper.x2,helper.x1
-        if helper.y1 > helper.y2: helper.y1, helper.y2 = helper.y2, helper.y1
         helper.image = meta.get_image().crop((helper.x1,helper.y1,helper.x2,helper.y2))
-        helper.selection = helper.x1,helper.y1,helper.x2,helper.y2
         helper.state=1
     if helper.state ==2:
         deltax = e.x - helper.mousex
@@ -77,11 +72,13 @@ def masterRelease(e,helper,meta):
         helper.state = 1
 
 def cut(helper,meta):
-    image = meta.get_image()
-    image.paste('white',helper.selection)
-    meta.draw(image)
-    helper.square = meta.canvas.create_rectangle(helper.x1, helper.y1, helper.x2, helper.y2)
-    helper.image_label = meta.canvas.create_image(helper.x1, helper.y1,anchor=NW,image=helper.TkImage)
+    if helper.state ==1:
+        print 'coords',helper.x1, helper.y1, helper.x2, helper.y2
+        to_paste = Image.new('RGB', (abs(helper.x2 - helper.x1), abs(helper.y2 - helper.y1)), '#ffffff')
+        new_im = meta.get_image()
+        new_im.paste(to_paste,(helper.x1, helper.y1, helper.x2, helper.y2))
+        print 'topaste', to_paste,'== new im',new_im
+        meta.draw(new_im)
 
 def copy(helper,meta):
     helper.image = meta.get_image().crop((helper.x1,helper.y1,helper.x2,helper.y2))
@@ -89,18 +86,13 @@ def copy(helper,meta):
 def paste(helper,meta):
     pass
 
-def finalize(helper,meta):
-    # paste image into meta image
-    image = meta.get_image()
-    image.paste(helper.image,(helper.x1, helper.y1, helper.x2, helper.y2))
-    meta.draw(image)
-    # clear canvas items
-    helper.clear(meta.canvas)
+def finalize():
+    pass
 
 def activate(c,meta):
     helper = helperObject()
     c.focus_set()
-    c.bind('<Button-1>',lambda e: masterClick(c,e,helper,meta))
+    c.bind('<Button-1>',lambda e: masterClick(c,e,helper))
     c.bind('<B1-Motion>',lambda e: masterPressedMotion(c,e,helper))
     c.bind('<ButtonRelease-1>',lambda e: masterRelease(e,helper,meta))
     c.bind('x',lambda e: cut(helper,meta))
